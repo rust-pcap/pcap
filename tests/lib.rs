fc4e0db2568b5d8da83b054e7e96fe38f4b8bd0c
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-03-10 01:13:50 +0000

    Use tempdir for savefile tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,11 +1,12 @@
 extern crate pcap;
 extern crate libc;
+extern crate tempdir;
 
-use pcap::{Active, Activated, Offline, Capture, Packet, PacketHeader, Linktype};
-use std::env;
-use std::fs;
 use std::ops::Add;
 use std::path::Path;
+use tempdir::TempDir;
+
+use pcap::{Active, Activated, Offline, Capture, Packet, PacketHeader, Linktype};
 
 #[test]
 fn read_packet_with_full_data() {
@@ -23,7 +24,6 @@ fn capture_from_test_file(file_name: &str) -> Capture<Offline> {
     let path = Path::new("tests/data/").join(file_name);
     Capture::from_file(path).unwrap()
 }
-
 
 #[test]
 fn unify_activated() {
@@ -103,18 +103,16 @@ fn capture_dead_savefile() {
     packets.push(1460408319, 1234, 1, 1, &[1]);
     packets.push(1460408320, 4321, 1, 1, &[2]);
 
-	  let mut tmp_file = env::temp_dir();
-	  tmp_file.push("pcap_dead_savefile_test.pcap");
+    let dir = TempDir::new("pcap").unwrap();
+    let tmpfile = dir.path().join("test.pcap");
 
     let cap = Capture::dead(Linktype(1)).unwrap();
-    let mut save = cap.savefile(&tmp_file).unwrap();
+    let mut save = cap.savefile(&tmpfile).unwrap();
     packets.foreach(|p| save.write(p));
     drop(save);
 
-    let mut cap = Capture::from_file(&tmp_file).unwrap();
+    let mut cap = Capture::from_file(&tmpfile).unwrap();
     packets.verify(&mut cap);
-
-	  fs::remove_file(&tmp_file).unwrap();
 }
 
 #[test]
@@ -128,21 +126,19 @@ fn capture_dead_savefile_append() {
     packets2.push(1460408322, 5432, 1, 1, &[4]);
     let packets = &packets1 + &packets2;
 
-	  let mut tmp_file = env::temp_dir();
-	  tmp_file.push("pcap_dead_savefile_append_test.pcap");
+    let dir = TempDir::new("pcap").unwrap();
+    let tmpfile = dir.path().join("test.pcap");
 
     let cap = Capture::dead(Linktype(1)).unwrap();
-    let mut save = cap.savefile(&tmp_file).unwrap();
+    let mut save = cap.savefile(&tmpfile).unwrap();
     packets1.foreach(|p| save.write(p));
     drop(save);
 
     let cap = Capture::dead(Linktype(1)).unwrap();
-    let mut save = cap.savefile_append(&tmp_file).unwrap();
+    let mut save = cap.savefile_append(&tmpfile).unwrap();
     packets2.foreach(|p| save.write(p));
     drop(save);
 
-    let mut cap = Capture::from_file(&tmp_file).unwrap();
+    let mut cap = Capture::from_file(&tmpfile).unwrap();
     packets.verify(&mut cap);
-
-	  fs::remove_file(&tmp_file).unwrap();
 }
