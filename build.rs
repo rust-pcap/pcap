92b6b814321e41d712c299efc16781263300d408
Ivan Smirnov <aldanor@users.noreply.github.com>
2017-03-13 13:34:53 +0000

    Use rustc-link-search=native for PCAP_LIBDIR

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use std::env;
 
 fn main() {
     if let Ok(libdir) = env::var("PCAP_LIBDIR") {
-        println!("cargo:rustc-link-search={}", libdir);
+        println!("cargo:rustc-link-search=native={}", libdir);
     }
 }
