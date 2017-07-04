7f2ec95446c2ec7e5dc574b051913ea5b91c2c9b
Stephan Buys <stephan.buys@panoptix.co.za>
2017-07-04 17:48:00 +0200

    Missing example file

---
+++ examples/streamlisten.rs
@@ -0,0 +1,39 @@
+extern crate pcap;
+extern crate futures;
+extern crate tokio_core;
+
+use pcap::{Capture, Packet, Error};
+use pcap::tokio::PacketCodec;
+use tokio_core::reactor::Core;
+use futures::stream::Stream;
+
+pub struct SimpleDumpCodec;
+
+impl PacketCodec for SimpleDumpCodec{
+    type Type = String;
+
+    fn decode<'p>(&mut self, packet: Packet<'p>) -> Result<Self::Type, Error> {
+        Ok(format!("{:?}", packet))
+
+    }
+}
+
+fn ma1n() -> Result<(),Error> {
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let cap = Capture::from_device("en0")?.open()?.setnonblock()?;
+    let s = cap.stream(&handle, SimpleDumpCodec{})?;
+    let done = s.for_each(move |s| {
+        println!("{:?}", s);
+        Ok(())
+    });
+    core.run(done).unwrap();
+    Ok(())
+}
+
+fn main() {
+    match ma1n() {
+        Ok(()) => (),
+        Err(e) => println!("{:?}", e),
+    }
+}
