177521aba93beb85904d12cb51a070e5dcfc48d9
Nils O. Selåsdal <nos@utel.tech>
2022-05-07 11:56:25 +0200

    Add back minor/major_version()

--- tests/lib.rs
+++ tests/lib.rs
@@ -345,4 +345,6 @@ fn test_pcap_version() {
     let capture = capture_from_test_file("packet_snaplen_65535.pcap");
 
     assert_eq!(capture.version(), (2, 4));
+    assert_eq!(capture.major_version(), 2);
+    assert_eq!(capture.minor_version(), 4);
 }
