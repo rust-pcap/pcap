affe44c8662c75f6641519e5ec9291c2ff3c4794
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2022-08-14 09:42:35 +0200

    Fix clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -88,7 +88,7 @@ mod raw;
 pub mod stream;
 
 /// An error received from pcap
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     /// The underlying library returned invalid UTF-8
     MalformedError(std::str::Utf8Error),
