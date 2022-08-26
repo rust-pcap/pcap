15c8b32339f2f2ce390dfe2ba242926cd3e2f212
qj <60492138+qrnch-jan@users.noreply.github.com>
2022-08-26 03:27:10 +0200

    Don't use c_uint in public interface. (#268)
    * Don't use c_uint in public interface.
    
    * Documentation updates for sendqueue.
    
    * Eliminate another c_uint from public interface.
    
    Co-authored-by: Jan Danielsson <jan@void>

--- src/sendqueue.rs
+++ src/sendqueue.rs
@@ -1,7 +1,12 @@
+//! WinPcap/npcap sendqueue support module.
+//!
+//! Sending individual packets through WinPcap/npcap can be stunningly slow, since a user-to-kernel
+//! transition is required for each packet transfer.  To alleviate this there's support for
+//! queueing up a batch of packets in userland, requiring only a single transition to kernel to
+//! transmit them all.
+
 use std::convert::TryInto;
 use std::ptr::NonNull;
-
-use libc::c_uint;
 
 use crate::raw;
 use crate::Error;
@@ -15,14 +20,18 @@ pub enum Sync {
 }
 
 impl SendQueue {
-    pub fn new(memsize: c_uint) -> Result<Self, Error> {
+    /// Create a new `SendQueue` object with a maximum capacity of `memsize`.
+    ///
+    /// The buffer size `memsize` must be able to contain both packet headers and actual packet
+    /// contents.
+    pub fn new(memsize: u32) -> Result<Self, Error> {
         let squeue = unsafe { raw::pcap_sendqueue_alloc(memsize) };
         let squeue = NonNull::new(squeue).ok_or(Error::InsufficientMemory)?;
 
         Ok(Self(squeue))
     }
 
-    pub fn maxlen(&self) -> c_uint {
+    pub fn maxlen(&self) -> u32 {
         unsafe { self.0.as_ref().maxlen }
     }
 
@@ -30,11 +39,14 @@ impl SendQueue {
         self.len() == 0
     }
 
-    pub fn len(&self) -> c_uint {
+    pub fn len(&self) -> u32 {
         unsafe { self.0.as_ref().len }
     }
 
     /// Add a packet to the queue.
+    ///
+    /// The `ts` argument only needs to be a `Some()` value if the transmission mode will be
+    /// synchronous when calling [`SendQueue::transmit()`].
     pub fn queue(&mut self, ts: Option<std::time::Duration>, buf: &[u8]) -> Result<(), Error> {
         let caplen = buf.len().try_into().ok().ok_or(Error::BufferOverflow)?;
         let len = buf.len().try_into().ok().ok_or(Error::BufferOverflow)?;
@@ -42,7 +54,7 @@ impl SendQueue {
         let pkthdr = raw::pcap_pkthdr {
             ts: if let Some(ts) = ts {
                 libc::timeval {
-                    // tv_sec is is currently i32 in libc when building for Windows
+                    // tv_sec is currently i32 in libc when building for Windows
                     tv_sec: ts.as_secs() as i32,
                     tv_usec: ts.subsec_micros() as i32,
                 }
@@ -67,7 +79,11 @@ impl SendQueue {
 
     /// Transmit the contents of the queue.
     ///
-    /// If entire queue was transmitted successfull the queue will be automatically reset.
+    /// If entire queue was transmitted successfully the queue will be automatically reset.
+    ///
+    /// If `sync` is set to `Sync::On` the difference between packet header timestamps
+    /// will be used as a delay between sending each packet.  If `Sync::Off` is used the packets
+    /// will be transmitted with no delay between packets.
     pub fn transmit(&mut self, dev: &mut Capture<Active>, sync: Sync) -> Result<(), Error> {
         let res = unsafe {
             raw::pcap_sendqueue_transmit(dev.handle.as_ptr(), self.0.as_ptr(), sync as i32)
