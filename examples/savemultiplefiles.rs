22beefcec20f9ecf29baa3ac28f7bdfc71d59cf8
Roman Beltiukov <maybe.hello.world@gmail.com>
2023-06-22 19:33:10 -0700

    Update savemultiplefiles.rs

--- examples/savemultiplefiles.rs
+++ examples/savemultiplefiles.rs
@@ -15,17 +15,21 @@ fn main() {
     let linktype = cap.get_datalink();
 
     // Save each 30 packets into a new PCAP file
-    let mut counter = 0;
-    loop {
+    let mut counter: usize = 0;
+
+    // For example purposes we will only save 5 files...
+    for _ in 0..5 {
         let mut save_file = Capture::dead(linktype)
             .unwrap()
             .savefile(format!("dump_{}.pcap", counter))
             .unwrap();
 
+        // ...30 packets each
         for _ in 0..30 {
             let packet = cap.next_packet().unwrap();
             save_file.write(&packet);
         }
+        save_file.flush().unwrap();
 
         counter += 1;
     }
