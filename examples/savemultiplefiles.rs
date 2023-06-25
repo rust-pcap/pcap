8e3c8ba23f5ec94bf90b8fef98c4239fd9a9a86b
Roman Beltiukov <maybe.hello.world@gmail.com>
2023-06-25 01:44:48 -0700

    Add example with saving packets to different files (#302)
    * Create savemultiplefiles.rs
    
    * Update savemultiplefiles.rs
    
    * Update savemultiplefiles.rs

---
+++ examples/savemultiplefiles.rs
@@ -0,0 +1,31 @@
+use pcap::Capture;
+
+fn main() {
+    // get the default Device
+    let device = pcap::Device::lookup().unwrap().unwrap();
+
+    // Setup Capture
+    let mut cap = pcap::Capture::from_device(device)
+        .unwrap()
+        .immediate_mode(true)
+        .open()
+        .unwrap();
+
+    // remember linktype to create PCAP files later
+    let linktype = cap.get_datalink();
+
+    // For example purposes we will only save 5 files...
+    for counter in 0..5 {
+        let mut save_file = Capture::dead(linktype)
+            .unwrap()
+            .savefile(format!("dump_{}.pcap", counter))
+            .unwrap();
+
+        // ...30 packets each
+        for _ in 0..30 {
+            let packet = cap.next_packet().unwrap();
+            save_file.write(&packet);
+        }
+        save_file.flush().unwrap();
+    }
+}
